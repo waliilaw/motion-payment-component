8bcb862cc9d1caa8f3b1566439bc9198bb216276
Wali <wali@Walis-MacBook-Air.local>
2025-06-26 16:52:32 +0530

    b

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { AnimatedText } from './components/animated-text'
+
+function App() {
+
+
+
+  return (
+
+    <div className='flex items-center justify-center min-h-screen bg-black'>
+   <AnimatedText />
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/components/animated-text.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const AnimatedText = () => {
+
+const text = "Hello , I am Wali" 
+
+  return (
+    <div className='text-white max-w-4xl mx-auto font-bold text-4xl'>
+        {text}
+    </div>
+  )
+}
